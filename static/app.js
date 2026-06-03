559cb5ff12910bfd7f959d866949750dddc4f6a0
flag-sudo <tm960077@gmail.com>
2026-06-03 19:46:35 +0300

    Update app.js

--- static/app.js
+++ static/app.js
@@ -4,78 +4,69 @@ tg.expand();
 let user = tg.initDataUnsafe?.user;
 let username = user ? (user.username || user.first_name) : "guest";
 
-
 async function createLot(){
-
-    const data = {
-        title: title.value,
-        desc: desc.value,
-        price: price.value,
-        blitz: blitz.value,
-        step: step.value,
-        time: time.value,
-        seller: username
-    };
 
     await fetch("/create_lot", {
         method:"POST",
         headers:{"Content-Type":"application/json"},
-        body: JSON.stringify(data)
+        body: JSON.stringify({
+            title:title.value,
+            desc:desc.value,
+            price:price.value,
+            blitz:blitz.value,
+            step:step.value,
+            time:time.value,
+            seller:username
+        })
     });
 
     location.reload();
 }
 
-
 async function bid(id){
-
     await fetch(`/bid/${id}`, {
         method:"POST",
         headers:{"Content-Type":"application/json"},
-        body: JSON.stringify({user: username})
+        body: JSON.stringify({user:username})
     });
-
     location.reload();
 }
 
-
 async function customBid(id){
 
-    let v = prompt("Цена:");
+    let v = prompt("price");
 
     await fetch(`/custom_bid/${id}`, {
         method:"POST",
         headers:{"Content-Type":"application/json"},
-        body: JSON.stringify({user: username, value:Number(v)})
+        body: JSON.stringify({user:username, value:Number(v)})
     });
 
     location.reload();
 }
-
 
 async function blitz(id){
 
     await fetch(`/blitz/${id}`, {
         method:"POST",
         headers:{"Content-Type":"application/json"},
-        body: JSON.stringify({user: username})
+        body: JSON.stringify({user:username})
     });
 
     location.reload();
 }
 
-
 async function unlock(id){
 
-    const res = await fetch(`/unlock/${id}`, {
+    const r = await fetch(`/unlock/${id}`, {
         method:"POST",
         headers:{"Content-Type":"application/json"},
-        body: JSON.stringify({user: username})
+        body: JSON.stringify({user:username})
     });
 
-    const data = await res.json();
+    const d = await r.json();
 
-    if(data.ok){
-        alert("Продавец: " + data.seller);
+    if(d.ok){
+        alert("seller: " + d.seller);
     }
 }
