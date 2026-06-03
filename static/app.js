f29f1cd27ef9041ce28f0b1f445d75265e457036
flag-sudo <tm960077@gmail.com>
2026-06-03 17:09:10 +0300

    17:09
    Refactor bid function to use template literals and update innerHTML for leader display.

--- static/app.js
+++ static/app.js
@@ -2,34 +2,22 @@ const tg = window.Telegram.WebApp;
 tg.expand();
 
 let user = tg.initDataUnsafe?.user;
-
-let username = "guest";
-
-if (user) {
-    username = user.username || user.first_name;
-}
+let username = user ? (user.username || user.first_name) : "guest";
 
 async function bid(lotId, button){
 
     const res = await fetch(`/bid/${lotId}`, {
         method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify({
-            user: username
-        })
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify({user: username})
     });
 
     const data = await res.json();
 
     if(data.ok){
-
         const lot = button.closest(".lot");
 
-        lot.querySelector(".price").innerHTML =
-            "💰 Ставка: <b>" + data.price + " грн</b>";
-
-        alert("👑 Лидер: " + data.leader);
+        lot.querySelector(".info").innerHTML +=
+            `<div>👑 Лидер: <b>${data.leader}</b></div>`;
     }
 }
