fa716bb4b2e5c5b84f53e1969fc2116502f5d181
flag-sudo <tm960077@gmail.com>
2026-06-03 17:07:13 +0300

    17:07
    Added auto-creation of a lot if the lots table is empty.

--- database.py
+++ database.py
@@ -7,19 +7,37 @@ cursor = conn.cursor()
 cursor.execute("""
 CREATE TABLE IF NOT EXISTS lots(
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-
     title TEXT,
     description TEXT,
-
     current_price INTEGER,
     blitz_price INTEGER,
-
     seller TEXT,
-
-    leader TEXT DEFAULT NULL,
-
-    end_time INTEGER DEFAULT (strftime('%s','now') + 3600)
+    leader TEXT,
+    end_time INTEGER
 )
 """)
 
 conn.commit()
+
+
+# -------------------------
+# АВТО-СОЗДАНИЕ ЛОТА ЕСЛИ ПУСТО
+# -------------------------
+cursor.execute("SELECT COUNT(*) FROM lots")
+count = cursor.fetchone()[0]
+
+if count == 0:
+    cursor.execute("""
+    INSERT INTO lots(title, description, current_price, blitz_price, seller, leader, end_time)
+    VALUES(?,?,?,?,?,?,?)
+    """, (
+        "Test iPhone 14",
+        "Почти новый телефон",
+        500,
+        1000,
+        "@seller",
+        "",
+        int(time.time()) + 3600
+    ))
+
+    conn.commit()
